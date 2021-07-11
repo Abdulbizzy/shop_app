fa9b3f1f729fcd8b8dca72dfd3aa82fb574768be
Abdulbizzy <abdulbasitsulaiman@yahoo.com>
2021-07-11 14:54:35 +0100

    a little update to see cart

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:shop_app/screens/orders_screen.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 import './providers/products_provider.dart';
+import './screens/user_products_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -39,6 +40,7 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserproductsScreen.routeName: (ctx) => UserproductsScreen(),
         },
       ),
     );
